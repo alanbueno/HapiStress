9c9eecdfdf5774437b2b2b60673245e87f498d30
Alan Bueno <alan.ernetwork@luizalabs.com>
2017-07-24 19:18:14 -0300

    Colocando processamento de lista!

--- libs/core/stressRoute.js
+++ libs/core/stressRoute.js
@@ -1,25 +1,27 @@
 module.exports = (server) => {
-    server.route(
-        {
-            method: 'POST',
-            path: '/stress',
-            config: trataRequest
-        }
-    )
+  server.route(
+    {
+      method: 'POST',
+      path: '/stress',
+      config: trataRequest
+    }
+  )
 }
 
 let stressController = require('./stressController.js'),
-    Joi = require('joi');
+  Joi = require('joi');
 
 let trataRequest = {
-    handler: stressController.Main,
-    validate: {
-        payload: {
-            url: Joi.string().required(),
-            concurrent: Joi.string().required(),
-            responseWaittime: Joi.string().required(),
-            threads: Joi.string().required(),
-            force: Joi.string().optional()
-        }
+  handler: stressController.Main,
+  validate: {
+    payload: {
+      url: Joi.string().required(),
+      count: Joi.string().optional(),
+      concurrent: Joi.string().optional(),
+      responseWaittime: Joi.string().required(),
+      threads: Joi.string().required(),
+      force: Joi.string().optional(),
+      dynamicParam: Joi.string().optional()
     }
+  }
 }
--- libs/core/stressService.js
+++ libs/core/stressService.js
@@ -1,24 +1,54 @@
 module.exports = {
-    StartStress: StartStress
+  StartStress: StartStress
 }
 
 let execAsyncClass = require('async-child-process'),
-    env = require('../../environment/environment.js');
+  env = require('../../environment/environment.js');
 
 async function StartStress(req, reqReply) {
-    try {
-        let result = await execAsyncClass.execAsync('stresser ' +
-            req.payload.url +
-            ' -c ' + req.payload.concurrent +
-            ' -t ' + req.payload.responseWaittime +
-            ' --html=' + env.logPath + '/report-$(date +%s).html' +
-            ' --threads=' + req.payload.threads // +
-            // ' --force' +
-            // ' --verbose c'
-        );
+  try {
+    if (req.payload.url.includes("@")) {
 
-        reqReply(result.stderr).code(200);
-    } catch (error) {
-        reqReply(error).code(500);
+      let iterations = req.payload.dynamicParam.split(';');
+      let result, url, index, command;
+
+      for (index = 0; index < iterations.length; index++) {
+        if (!iterations[index].toString())
+          continue;
+
+        url = req.payload.url.replace('@',
+          iterations[index].toString());
+
+        command = 'stresser \"' +
+          url +
+          '\" -n 1' +
+          ' -c 1' +
+          ' -t ' + req.payload.responseWaittime +
+          ' -m GET' +
+          // ' --html=' + env.logPath + 'report-$(date +%s).html' +
+          ' --threads=' + req.payload.threads +
+          ' --force';
+        result = await execAsyncClass.execAsync(command);
+      }
+
+      reqReply("Total de cpf's acionados: " + index).code(200);
+      return;
     }
+
+    let result = await execAsyncClass.execAsync('stresser \"' +
+      req.payload.url +
+      '\" -n ' + req.payload.count +
+      ' -c ' + req.payload.concurrent +
+      ' -t ' + req.payload.responseWaittime +
+      ' -m GET' +
+      ' --html=' + env.logPath + 'report-$(date +%s).html' +
+      ' --threads=' + req.payload.threads +
+      ' --force '
+      // ' --verbose c'
+    );
+
+    reqReply(result.stderr).code(200);
+  } catch (error) {
+    reqReply(error.message.toString()).code(500);
+  }
 }
